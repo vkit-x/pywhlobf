c1c9609eef766a6e0be31cf93593586cb405e8a4
Hunt Zhan <huntzhan.dev@gmail.com>
2021-09-06 14:55:22 +0800

    decoration chagne.

--- dockerfile/entrypoint.sh
+++ dockerfile/entrypoint.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
+trap "echo 'error: Script failed: see failed command above'" ERR
 
 fixuid -q
 
 if [ -z "$PYTHON_ABI_TAG" ] ; then
-    echo "PYTHON_ABI_TAG not set (should be one of (cp36-cp36m, cp37-cp37m, cp38-cp38, cp39-cp39)), abort."
+    echo "PYTHON_ABI_TAG not set."
+    echo "Should be one of (cp35-cp35m, cp36-cp36m, cp37-cp37m, cp38-cp38, cp39-cp39))."
+    echo "Abort."
     exit 1
 fi
 
---
+++ dockerfile/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+trap "echo 'error: Script failed: see failed command above'" ERR
+
+declare -a PYTHON_ABIS=(
+    # EOL.
+    cp35-cp35m
+    cp36-cp36m
+    cp37-cp37m
+    cp38-cp38
+    cp39-cp39
+)
+
+for PYTHON_ABI in $PYTHON_ABIS ; do
+    echo "Installing pywhlobf to ${PYTHON_ABI}."
+    PIP_FILE="/opt/python/${PYTHON_ABI}/bin/pip"
+    if [[ ! -f "$PIP_FILE" ]] ; then
+        echo "${PYTHON_ABI} not found, skip."
+        continue
+    fi
+    "$PIP_FILE" install --no-cache-dir -U pip
+    "$PIP_FILE" install --no-cache-dir pywhlobf=="$PYWHLOBF_VERSION"
+done
